5220add6c063336d7c57c0281ea4e2acc2897e62
Tomislav Fabeta <tomislav.fabeta@degordian.com>
2018-01-26 14:00:29 +0100

    exported testing modules

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports.mocha = require('mocha');
+module.exports.chai = require('chai');
+module.exports.sinon = require('sinon');
+module.exports.sinonchai = require('sinon-chai');
+module.exports.sinontest = require('sinon-test');
