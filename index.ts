496c780569f897089ecdd0ad790bf704bfdff045
Tomislav Fabeta <tomislav.fabeta@degordian.com>
2018-01-26 15:09:52 +0100

    export ts modules

---
+++ index.ts
@@ -0,0 +1,13 @@
+import sinontest from "sinon-test";
+import mocha from "mocha";
+import chai from "chai";
+import sinon from "sinon";
+import sinonchai from "sinon-chai";
+
+export default {
+    sinon: sinon,
+    chai: chai,
+    mocha: mocha,
+    sinontest: sinontest,
+    sinonchai: sinonchai,
+};
